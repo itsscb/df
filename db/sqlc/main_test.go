b88be701bbf8797046bc2f4e816c010b7ca88da8
itsscb <dev@itsscb.de>
2023-09-21 19:18:18 +0200

    add dynamic configuration using viper (#16)

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,18 +6,18 @@ import (
 	"os"
 	"testing"
 
+	"github.com/itsscb/df/util"
 	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/df?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatalf("could not connect to DB: %s", err)
 	}
