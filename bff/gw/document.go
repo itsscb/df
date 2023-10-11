3d6d87854ed88808e9ff9524ed8e082dc4a2f7e9
itsscb <dev@itsscb.de>
2023-10-12 00:30:50 +0200

    ft/adds docker-compose volumes prepares fileupload
    - Files should be saved into subdirs

--- bff/gw/document.go
+++ bff/gw/document.go
@@ -11,6 +11,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// type uploadDocumentRequest struct {
+// 	PersonID uint64 `json:"person_id"`
+// 	MailID   uint64 `json:"mail_id"`
+// }
+
 func (server *Server) UploadDocument(ctx *gin.Context) {
 	authHeader := ctx.GetHeader("authorization")
 
@@ -35,13 +40,36 @@ func (server *Server) UploadDocument(ctx *gin.Context) {
 		return
 	}
 
+	// TODO: FileUpload with POST-Values
+	// bodyData, _ := io.ReadAll(ctx.Request.Body)
+	// slog.Info("Document", slog.String("body", fmt.Sprintf("%#v", string(bodyData))))
+
 	file, err := ctx.FormFile("file")
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(errors.New("could not parse file")))
 		return
 	}
 
-	p := filepath.Join("./files", fmt.Sprintf("%d", account.ID), file.Filename)
+	targetDir := filepath.Join("./files", fmt.Sprintf("%d", account.ID))
+
+	// var req *uploadDocumentRequest
+	// _ = ctx.ShouldBindJSON(&req)
+
+	// if req != nil {
+	// 	if req.MailID <= 0 && req.PersonID <= 0 {
+	// 		ctx.JSON(http.StatusBadRequest, errorResponse(errors.New("document can't be assigned to both person_id AND mail_id")))
+	// 		return
+	// 	}
+
+	// 	if req.MailID > 0 {
+	// 		targetDir = filepath.Join(targetDir, "mail", fmt.Sprintf("%d", req.MailID))
+	// 	}
+	// 	if req.PersonID > 0 {
+	// 		targetDir = filepath.Join(targetDir, "person", fmt.Sprintf("%d", req.PersonID))
+	// 	}
+	// }
+
+	p := filepath.Join(targetDir, file.Filename)
 
 	if _, err := os.Stat(p); err != nil {
 		err = ctx.SaveUploadedFile(file, p)
